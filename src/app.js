8e28a360c759366764909ceed9f9a94ea8002747
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-03-15 16:42:28 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+var who = ["The dog", "My grandma", "His turtle", "My bird"];
+var action = ["ate", "peed", "crushed", "broke"];
+var what = ["my homework", "the keys", "the car"];
+var when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+var randomNumber = 0;
+var randomWho = "";
+var randomAction = "";
+var randomWhat = "";
+var randomWhen = "";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let newExcuse = buildExcuse();
+
+  document.getElementById("excuse").innerHTML = newExcuse;
+  document.getElementById("excuse").style.visibility = "visible";
+
+  //console.log("Hello Rigo from the console!");
 };
+
+function buildExcuse() {
+  randomNumber = Math.floor(Math.random() * 4);
+  randomWho = who[randomNumber];
+  randomNumber = Math.floor(Math.random() * 4);
+  randomAction = action[randomNumber];
+  randomNumber = Math.floor(Math.random() * 3);
+  randomWhat = what[randomNumber];
+  randomNumber = Math.floor(Math.random() * 5);
+  randomWhen = when[randomNumber];
+
+  return randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+}
